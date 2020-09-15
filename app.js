8e3b7efe5022c695c234a513c96d1f4030a6aa71
TJ <tahmeenaowais@yahoo.com>
2020-09-15 18:37:35 -0400

    updates

--- app.js
+++ app.js
@@ -2,138 +2,142 @@
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const Table= require("cli-table")
+const Table = require("cli-table")
 
 
 //setting connection with MySQL
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "Falak@2019",
-  database: "employeetracker_db"
+    // Your password
+    password: "Falak@2019",
+    database: "employeetracker_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-     createTables();
-  });
-  
+    afterConnection();
+});
 
-function createTables(){
+
+function createTables() {
     const table = new Table({
         head: ['ID', 'title'],
         colWidths: [50, 50]
     });
     connection.query("CREATE TABLE department (id INT PRIMARY KEY, title VARCHAR(30))"),
-    function(err, res) {
-        if (err) throw err;
-        console.log("success");}
+        function (err, res) {
+            if (err) throw err;
+            console.log("success");
+        }
     connection.query("INSERT INTO department(id,title) VALUES (433 ,'developer');"),
-  
-    function(err, res) {
-        if (err) throw err;
-        console.log("success");}
 
-    table.push ([433,"Developer"]);
+        function (err, res) {
+            if (err) throw err;
+            console.log("success");
+        }
+
+    table.push([433, "Developer"]);
     console.log(table.toString());
-     
+
 }
-     
-     
 
+function afterConnection() {
 
-// function readProducts() {
-//     console.log("Selecting all products...\n");
+    inquirer.prompt([{
 
-//     connection.query("SELECT * FROM role", function(err, res) {
-//         if (err) throw err;
-//         console.log(res);
-//         //updateProduct()
-//       });
-//     }
-// function showTable(){
-//     connection.query("show tables;", function(err, res) {
-   
-//      if (err) throw err;
-//       // Log all results of the SELECT statement
-//       //shows tables
-//       console.log(res);
-//       //connection.end();
-      
-//    });const table = new Table({
-//     head: ['ID', 'First Name', 'Last Name','Role', 'Salary', 'Manager']
-//   , colWidths: [20, 20,20,20,20,20]
-// });
+        type: "list",
+        name: "response",
+        message: "What would you like to do?",
+        choices: [
+            "View all Employees by Department",
+            "View all Employees by Manager",
+            "View all Employees",
+            "Add Employee",
+            "Delete Employee",
+            "Update Employee"
+        ]
 
-// // table is an Array, so you can `push`, `unshift`, `splice` and friends
-// for (i=0; i<3; i++){
-// table.push(
-//     [1+i, 'Tahmeena', 'Javed','developer', '150000', 'todd']
-// );}
-// console.log(table.toString());}
-   
-  
-//   afterConnection()
-// function afterConnection(){
+    }]).then(function (data) {
 
-//     inquirer.prompt([{
+        console.log(data.response)
+        switch(data.response) {
+            case "View all Employees by Department":
+            getDepartment();
+            break;
+            case "View all Employees": 
+            getAll()
+            break; 
+            case "Delete Employee":
+            removeEmployee();
+            break;
+        }
+        
+    })
+}
 
+function getDepartment() {
+    connection.query("select * from department", function (err, data) {
+        if (err) throw err;
+        console.table(data)
+        afterConnection(); 
+    })
+}
+
+function removeEmployee() {
+    connection.query("select * from employee", function (err, data) {
        
-//         //asks Department ID of Team member
-//         type: "checkbox",
-//         name: "response",
-//         message: "What would you like to view?",
-//         choices:[
-// "View  all Employees by Department",
-// "View all Employees by Manager",
-// "View all Employees"
+        // if (err) throw err;
+        // console.table(data)
+
+        const names = data.map(employee => {
+            return {
+                name: `${employee.first_name} ${employee.last_name}`,
+                id: employee.id
+            }
+       
+    })
+
+    inquirer.prompt([{
+
+        type: "list",
+        name: "deleteName",
+        message: "What would you like to do?",
+        choices: names
+
+    }]).then(response => {
+        console.log(response.deleteName)
+    })
 
 
-//         ]
-                  
-//     },
-// ]).then(function (data) {
 
-//     if(data.response =="View  all Employees by Department"){
-//         getDepartment()
-//         }
-           
-//           if (data.response =="View all Employees"){
-//           getAll()
+   
 
-//           }
-//           else{
-//               getManager()
-//           }         
-//         })
-//         function getDepartment(){
-//             connection.query("select* from department")
-//             console.log("success")
-          
+}); 
 
-//         }
-//         function  getAll(){
-//             connection.query("select* from employee")
-//             console.log("All")
-//             showTable()
-//             readProducts();
+}
 
-      
+function getAll() {
+    connection.query("select * from employee", function (err, data) {
+        if (err) throw err;
+        console.table(data)
+        afterConnection(); 
+    })
+}
 
-//         }
-// function getManager(){
-//     connection.query("select* from role")
-//     console.log("Manager")
-
-// }
-//     }
+function getManager() {
+    connection.query("select * from role", function (err, data) {
+        if (err) throw err;
+        console.table(data)
+        afterConnection(); 
+    })
+};
