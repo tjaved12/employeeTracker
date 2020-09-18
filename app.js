17c1cc723f19a7c4bfebe7acb7ad891900b866b2
TJ <tahmeenaowais@yahoo.com>
2020-09-17 21:28:09 -0400

    added logo for app

--- app.js
+++ app.js
@@ -4,6 +4,28 @@ const table = require('console.table');
 
 //setting connection with MySQL
 var mysql = require("mysql");
+//require Cfonts
+const CFonts = require('cfonts');
+
+
+// Welcome Menu
+// https://www.npmjs.com/package/cfonts for logo and ref
+console.log(
+    CFonts.say('Employee|Tracker|Application!', {
+        font: 'simple3d',             // define the font face
+        align: 'center',            // define text alignment
+        colors: ['#f20'],          // define all colors
+        background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
+        letterSpacing: 1,           // define letter spacing
+        lineHeight: 1,              // define the line height
+        space: true,                // define if the output text should have empty lines on top and on the bottom
+        maxLength: '0',             // define how many character can be on one line
+        gradient: true,             // define your two gradient colors
+        independentGradient: false, // define if you want to recalculate the gradient for each new line
+        transitionGradient: false,  // define if this is a transition between colors directly
+        env: 'node'                 // define the environment CFonts is being executed in
+    })
+);
 
 var connection = mysql.createConnection({
     host: "localhost",
